90c0374edee2053cf25b666fd976e93444b9c689
Nimeshkamihiran <nimeshmihiranga183@gmail.com>
2025-07-11 18:22:25 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "nimeshmihiranga183@gmail.com",
-  password: "Nimesh@123",
+  email: "mihirangamihiranga223@gmail.com",
+  password: "Nimesh12345678",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
